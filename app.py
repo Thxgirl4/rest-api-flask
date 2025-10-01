635671050cc3595efec2429356c6f9c819d8311d
Thxgirl4 <anajuliasilva3304@gmail.com>
2025-10-01 10:36:36 -0300

    fix: formatting according to flake8

--- app.py
+++ app.py
@@ -4,18 +4,21 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
-    
+
+
 class Users(Resource):
     def get(self):
-        return{"message": "user 1"}
-    
+        return {"message": "user 1"}
+
+
 class User(Resource):
     def get(self):
         return {"message": "CPF"}
-    
+
     def post(self):
         return {"message": "teste"}
 
