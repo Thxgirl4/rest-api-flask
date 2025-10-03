87e170921b226740363b452189493a395762b1b8
Thxgirl4 <anajuliasilva3304@gmail.com>
2025-10-02 22:35:17 -0300

    feat: add mongoengine

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 api = Api(app)
+db = MongoEngine(app)
+
+
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'users',
+    'host': 'mongodb',
+    'port': 27017,
+    'user': 'admin',
+    'password': 'admin'
+}
+
+class UserModel(db.Document):
+    cpf = db.StringField(required=True, unique=True)
+    email = db.StringField(required=True)
+    first_name = db.StringField(max_length=50)
+    last_name = db.StringField(max_length=50)
+    birth_date = db.DateTimeField(required=True)
 
 
 class HelloWorld(Resource):
@@ -12,7 +30,8 @@ class HelloWorld(Resource):
 
 class Users(Resource):
     def get(self):
-        return {"message": "user 1"}
+        return UserModel.objects()
+        #return {"message": "user 1"}
 
 
 class User(Resource):
